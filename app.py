ab5ec9afe809dc914a9fe8ca1ed94d4f3eff4d7d
OsamaDW <s12028609@stu.najah.edu>
2024-03-13 18:38:22 +0200

    add purchase inforfmation in order table

--- app.py
+++ app.py
@@ -3,29 +3,38 @@ import sqlite3
 import requests as requests
 from flask import Flask, request, jsonify
 
+def get_db_connection():
+    conn = sqlite3.connect('orders.db')
+    conn.row_factory = sqlite3.Row
+    return conn
 
 app = Flask(__name__)
 
-@app.route('purchase/<param1>' , methods = ['GET'])
+@app.route('/purchase/<param1>' , methods = ['GET'])
 def orders(param1):
-    base_url = 'http://localhost:5050/query'
+    base_url = 'http://localhost:5000/query'
     response = requests.get(base_url, params={'item_number': param1})  # Ensuring proper URL encoding
 
     if response.ok:
         data = response.json()
-        if data['count'] <= 0:
+        if data['Count'] <= 0:
             return jsonify({'purchase' : "this book is out of stock" })
-        response = requests.patch('http://localhost:5050/update' , json = {'stock_count' : -1
+
+        response = requests.patch('http://localhost:5000/update' , json = {'stock_count' : -1
                                                                 ,'item_number' : data['ItemNumber']})
-        if response == "Updated record successfully":
+        if response.json() == "Updated record successfully":
+            con = get_db_connection()
+            con.cursor().execute("Insert Into 'order' (item_number) values ("+param1+")")
+            con.commit()
             return jsonify({'purchase' : 'successfuly'})
         else:
-            return jsonify({'purchase': 'Failed'})
+            return jsonify({'purchase' : 'fail'})
     else:
         return jsonify({'error': 'Failed to fetch data'}), response.status_code
 
 
 
+
 if __name__ == '__main__':
-    app.run(debug=True ,host= '0.0.0.0' ,port=5060)
+    app.run(debug=True ,host= '0.0.0.0' ,port=5990)
 
